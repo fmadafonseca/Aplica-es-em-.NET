7337ea03945a9109353ed9e357d209464ec9ac1b
Fábio Martins <66891924+fmadafonseca@users.noreply.github.com>
2021-03-19 09:24:06 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace NoteApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.bttNwq = new System.Windows.Forms.Button();
+            this.bttSave = new System.Windows.Forms.Button();
+            this.bttRead = new System.Windows.Forms.Button();
+            this.bttDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Message";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(68, 9);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(68, 53);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(226, 356);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(316, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(472, 356);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // bttNwq
+            // 
+            this.bttNwq.Location = new System.Drawing.Point(68, 415);
+            this.bttNwq.Name = "bttNwq";
+            this.bttNwq.Size = new System.Drawing.Size(75, 23);
+            this.bttNwq.TabIndex = 5;
+            this.bttNwq.Text = "New";
+            this.bttNwq.UseVisualStyleBackColor = true;
+            this.bttNwq.Click += new System.EventHandler(this.bttNwq_Click);
+            // 
+            // bttSave
+            // 
+            this.bttSave.Location = new System.Drawing.Point(219, 415);
+            this.bttSave.Name = "bttSave";
+            this.bttSave.Size = new System.Drawing.Size(75, 23);
+            this.bttSave.TabIndex = 6;
+            this.bttSave.Text = "Save";
+            this.bttSave.UseVisualStyleBackColor = true;
+            this.bttSave.Click += new System.EventHandler(this.bttSave_Click);
+            // 
+            // bttRead
+            // 
+            this.bttRead.Location = new System.Drawing.Point(410, 415);
+            this.bttRead.Name = "bttRead";
+            this.bttRead.Size = new System.Drawing.Size(75, 23);
+            this.bttRead.TabIndex = 7;
+            this.bttRead.Text = "Read";
+            this.bttRead.UseVisualStyleBackColor = true;
+            this.bttRead.Click += new System.EventHandler(this.bttRead_Click);
+            // 
+            // bttDelete
+            // 
+            this.bttDelete.Location = new System.Drawing.Point(688, 415);
+            this.bttDelete.Name = "bttDelete";
+            this.bttDelete.Size = new System.Drawing.Size(75, 23);
+            this.bttDelete.TabIndex = 8;
+            this.bttDelete.Text = "Delete";
+            this.bttDelete.UseVisualStyleBackColor = true;
+            this.bttDelete.Click += new System.EventHandler(this.bttDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bttDelete);
+            this.Controls.Add(this.bttRead);
+            this.Controls.Add(this.bttSave);
+            this.Controls.Add(this.bttNwq);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button bttNwq;
+        private System.Windows.Forms.Button bttSave;
+        private System.Windows.Forms.Button bttRead;
+        private System.Windows.Forms.Button bttDelete;
+    }
+}
+
